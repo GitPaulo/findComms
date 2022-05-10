7fd5e77ac27c3a629c1cae8abedbdb34e8438c3a
Paulo Santos <paulo.santos@thehutgroup.com>
2022-05-10 19:55:04 +0100

    further work on frontend + firebase

--- frontend/src/twitter.service.ts
+++ frontend/src/twitter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "./environments/environment";
 
 export interface CommsData {
   users: any[];
@@ -15,7 +16,7 @@ export class TwitterService {
   constructor(private http: HttpClient) {}
 
   getCommsUsers(userId: string): Observable<CommsData> {
-    return this.http.get<CommsData>("/api/find", {
+    return this.http.get<CommsData>(`${environment.apiUrl}/api/find`, {
       params: { userIdentifier: userId },
     });
   }
