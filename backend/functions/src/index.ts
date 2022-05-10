4cd99380f53fa6ce975765fd5665c7e9f494cfb6
Paulo Santos <paulo.santos@thehutgroup.com>
2022-05-10 19:54:52 +0100

    further work on backend + firebase

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,188 @@
+import {
+  TwitterApi,
+  UserV1,
+  UserV2,
+  UserV2TimelineResult,
+} from "twitter-api-v2";
+import express, { Express, Request, Response } from "express";
+import * as functions from "firebase-functions";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+// ? this wont scale
+import memcache from "memory-cache";
+import termsMap from "./terms_map.json";
+
+dotenv.config();
+
+// Express
+const app: Express = express();
+const port = process.env.PORT;
+const production = process.env.PRODUCTION;
+// Twitter API
+const twitterClient = new TwitterApi(String(process.env.TWITTER_API_BEARER));
+const client = twitterClient.readWrite;
+// Memory Cache
+const cache = new memcache.Cache();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+type FindDomain = "followers" | "following" | "all";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface CommsData {
+  users: UserV1[];
+  terms: { [id: string]: string };
+}
+app.get("/api/find", async (req: Request, res: Response) => {
+  const userIdentifier = req.query.userIdentifier as string;
+  const findDomain = (req.query.domain as FindDomain) || "all";
+
+  // Log
+  console.log("/followers userIdentifier", userIdentifier);
+
+  if (!userIdentifier) {
+    return res.status(400).send("Invalid user identifier.");
+  }
+
+  // Resolve ID
+  let id: string;
+  try {
+    if (!isNaN(Number(userIdentifier.toString()))) {
+      id = userIdentifier;
+    } else {
+      id = (await client.v2.userByUsername(userIdentifier)).data.id;
+    }
+    console.log("Resolved ID: " + id);
+  } catch (e: any) {
+    console.log(e.stack);
+    return res.status(400).send("Could not find username.");
+  }
+
+  // Resolve domain
+  let domain: UserV2[] = [];
+  try {
+    /* ? mock
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(path.join(__dirname, './mock_data.json'));
+    return;
+    */
+    const cached = cache.get(`${id}_${findDomain}`);
+    if (cached) {
+      // Log
+      console.log("Returned cached value.");
+
+      res.setHeader("Content-Type", "application/json");
+      res.send(cached);
+      return;
+    }
+
+    switch (findDomain) {
+      case "followers":
+        domain = (await client.v2.followers(id, { max_results: 1000 }))?.data;
+        break;
+      case "following":
+        domain = (await client.v2.following(id, { max_results: 1000 }))?.data;
+        break;
+      case "all":
+        // API call removes duplicates
+        domain = [
+          ...((await client.v2.following(id, { max_results: 1000 }))?.data ||
+            []),
+          ...((await client.v2.followers(id, { max_results: 1000 }))?.data ||
+            []),
+        ];
+        break;
+      default:
+        return res.status(400).send("Bad usage of domain!");
+    }
+
+    // If they have no followers/following
+    if (!domain.length) {
+      res.setHeader("Content-Type", "application/json");
+      return res.send(JSON.stringify({ users: [], foundTerms: {} }));
+    }
+  } catch (e: any) {
+    console.log(e.stack);
+    if (e.response.code === 421) {
+      console.log("Likely twitter API Rate limit reached!");
+      return res.status(421).send("Rate limit reached?");
+    }
+    return res.status(500).send("Failed to resolve domain.");
+  }
+
+  // log size
+  console.log("Domain size: ", domain.length);
+
+  // Resolve domain users
+  let domainUsers: UserV1[] = [];
+  try {
+    let followerIds = domain.map((user: UserV2) => user.id);
+    followerIds = [...new Set(followerIds)];
+    console.log("Trimmed size: ", followerIds.length);
+
+    // API restriction
+    // TODO use POST?
+    const chunkSize = 100;
+    for (let i = 0; i < followerIds.length; i += chunkSize) {
+      const chunkIds = followerIds.slice(i, i + chunkSize);
+      domainUsers = [
+        ...domainUsers,
+        ...(await client.v1.users({ user_id: chunkIds })),
+      ];
+    }
+    console.log("Result size:", domainUsers.length);
+  } catch (e: any) {
+    console.log(e.stack);
+    return res.status(500).send("Failed to resolve follower user data.");
+  }
+
+  // Filter users by comms in descriptions
+  // TODO: more complex search
+  const openOrClosed: { [id: string]: "open" | "closed" | "unknown" } = {};
+  const foundTerms: { [id: string]: string[] } = {};
+  domainUsers = domainUsers.filter((user: UserV1) => {
+    for (const [keyTerm, terms] of Object.entries(termsMap)) {
+      const searchSpace =
+        user.name.toLowerCase() + user.description?.toLowerCase();
+      // terms
+      if (terms.some((term: string) => searchSpace.includes(term))) {
+        foundTerms[user.id] = foundTerms[user.id]
+          ? [...foundTerms[user.id], keyTerm]
+          : [keyTerm];
+      }
+      // open/closed
+      // TODO: improve
+      const open = searchSpace.includes("open");
+      const closed = searchSpace.includes("closed");
+      if (open) {
+        openOrClosed[user.id] = "open";
+      } else if (closed) {
+        openOrClosed[user.id] = "closed";
+      }
+
+      if (open && closed) {
+        openOrClosed[user.id] = "unknown";
+      }
+    }
+
+    return Boolean(foundTerms[user.id]?.length);
+  });
+
+  // Cache it, rate limits!!
+  const responseValue = JSON.stringify({
+    statuses: openOrClosed,
+    terms: foundTerms,
+    users: domainUsers,
+  });
+  cache.put(`${id}_${findDomain}`, responseValue, 300000);
+
+  // Respond
+  res.setHeader("Content-Type", "application/json");
+  res.send(responseValue);
+});
+
+exports.app = functions.https.onRequest(app);
